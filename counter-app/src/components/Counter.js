c4e2534cf3afccfe7fce1b6d8a74de50344e28b6
Stacey Teltser <stacey.teltser@gmail.com>
2020-06-15 16:10:36 -0500

    tried to implement color change but gave up so implememnted a message that says the click counter is greater than or less than 0

--- counter-app/src/components/Counter.js
+++ counter-app/src/components/Counter.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './app.scss';
 
+// const black = "#00000";
+// const green = "#45E37C";
+// const red = "#EC718B";
+
 class Counter extends React.Component {
     constructor(props){
       super(props);
   
       this.state = {
-        // words: 'click to increase counter',
         clicks: 0,
+        // color: black,
       };
+
+      this.changeColor = this.eachIncrementClick.bind(this);
     }
-  
+    
     handleClicks = e => {
       let clicks = e.target.value;
       this.setState({ clicks });
@@ -19,7 +25,7 @@ class Counter extends React.Component {
     eachIncrementClick = e => {
       e.preventDefault();
       let clicks = this.state.clicks += 1;
-  
+    //   const newColor = this.state.color === green;
       this.setState({ clicks });
     }
   
@@ -32,12 +38,15 @@ class Counter extends React.Component {
   
   
     render(){
+        let counterIndicator = this.state.clicks > 0 ? 'Greater than 0' : 'less than 0';
+
       return(
         <div className="wrapper">
-          <h2>Watch the counter go up and down!</h2>
+          <h2>Watch the counter go up and down:</h2>
           <h3>Count: {this.state.clicks} </h3>
           <button onClick={this.eachIncrementClick}>Increment</button>
           <button onClick={this.eachDecrementClick}>Decrement</button>
+          <div>The click counter is: {counterIndicator} </div>
         </div>
       )
     }
